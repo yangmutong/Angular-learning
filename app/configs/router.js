a93a3c92a2bee23971eebfe710ee7d185dea4089
yangmutong <985777876@qq.com>
2016-06-25 19:27:50 +0800

    add router config

--- app/configs/router.js
+++ app/configs/router.js
@@ -7,6 +7,55 @@
         .config(appConfig);
 
     function appConfig($stateProvider, $urlRouterProvider){
-        
+        $stateProvider.state('default', {
+            url:'',
+            templateUrl: 'controllers/home/index.html',
+            controller: 'HomeIndexController as vm'
+        });
+
+        $stateProvider.state('home', {
+            url: "/",
+            templateUrl: 'controllers/home/index.html',
+            controller: "HomeIndexController as vm"
+        }).state('notFound', {
+            url: "/notFound",
+            templateUrl: "controllers/home/notFound.html",
+            controller: "HomeNotFoundController as vm"
+        });
+
+        $urlRouterProvider.otherwise('/notFound');
+
+
+        $stateProvider.state('reader', {
+            url: "/reader",
+            template: '<div ui-view></div>',
+            abstract: true
+        }).state('reader.create', {
+            url: '/create',
+            tempalteUrl: "controllers/reader/create.html",
+            controller: "ReaderCreateController as vm"
+        }).state('reader.list', {
+            url: '/list',
+            templateUrl: "controllers/reader/list.html",
+            controller: "ReaderListController as vm"
+        });
+
+        $stateProvider.state('thread', {
+            url: '/thread',
+            template: '<div ui-view></div>',
+            abstract: true
+        }).state('thread.list', {
+            url: '/list',
+            templateUrl: 'controllers/thread/list.html',
+            controller: "ThreadListController as vm",
+        }).state('thread.tree', {
+            url: '/tree',
+            templateUrl: 'controllers/thread/tree.html',
+            controller: "ThreadTreeController as vm"
+        }).state('thread.show', {
+            url: '/:id/show?title&poster',
+            templateUrl: 'controllers/thread/show.html',
+            controller: 'ThreadShowController as vm'
+        })
     }
 })();
