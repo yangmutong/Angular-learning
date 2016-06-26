5811affbf90c747b2389a5cad7ab491d92c08d71
yangmutong <985777876@qq.com>
2016-06-26 14:42:35 +0800

    add tree

---
+++ app/services/ui/tree.js
@@ -0,0 +1,16 @@
+/**
+ * Created by MuTong Yang on 2016/6/26/0026.
+ */
+(function(){
+    "use strict";
+    angular.module('com.ngnice.app')
+        .service('tree', function(){
+            var self = this;
+            var enhanceItem = function(item, childrenName){
+                item.$hasChildren = function(){
+                    
+                }
+            }
+
+        })
+})();
