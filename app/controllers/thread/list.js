b7c17598fd6316e926bd8f30693343a2463283a5
yangmutong <985777876@qq.com>
2016-06-26 13:33:44 +0800

    add

--- app/controllers/thread/list.js
+++ app/controllers/thread/list.js
@@ -5,6 +5,25 @@
     "use strict";
     angular.module('com.ngnice.app')
         .controller('ThreadListController', function(){
+            var vm = this;
+            vm.items = [
+                {
+                    title: "first",
+                    poster: "yangmutong",
+                    dateCreated: '2016-06-26T00:00:00'
+                },
+                {
+                    title: "second",
+                    poster: "yangmutong",
+                    dateCreated: '2016-06-26T00:00:00'
+                },
+                {
+                    title: "third",
+                    poster: "yangmutong",
+                    dateCreated: '2016-06-26T00:00:00'
+                }
+            ];
+
 
         })
 })();
